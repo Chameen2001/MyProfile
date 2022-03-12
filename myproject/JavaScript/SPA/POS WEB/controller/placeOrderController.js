1be52ebdf55082b52d2c900ff62299e148ba2d8e
Chameen2001 <dilhara1117@gmail.com>
2022-03-12 23:31:30 +0530

    create a method named ClearAll to clear all textfiled by after placing order.

--- myproject/JavaScript/SPA/POS WEB/controller/placeOrderController.js
+++ myproject/JavaScript/SPA/POS WEB/controller/placeOrderController.js
@@ -1,20 +1,20 @@
 var refresh_customer_cmb = function () {
-  
   $("#cust-list").empty();
-  $("#cust-list").append(`<li class="cus-option"><img src="" alt=""/><p>None</p></li>`);
+  $("#cust-list").append(
+    `<li class="cus-option"><img src="" alt=""/><p>None</p></li>`
+  );
   for (let index = 0; index < customers.length; index++) {
     console.log("callded");
-    var raw =`<li class="cus-option">
+    var raw = `<li class="cus-option">
     <img
       src="https://w7.pngwing.com/pngs/419/473/png-transparent-computer-icons-user-profile-login-user-heroes-sphere-black-thumbnail.png"
       alt=""
     />
     <p>${customers[index].getName()} (${customers[index].getCID()})</p>
   </li>`;
-      
+
     $("#cust-list").append(raw);
     var cus_option = $(".cus-option");
-    
 
     for (const iterator of cus_option) {
       $(iterator).off();
@@ -23,14 +23,11 @@ var refresh_customer_cmb = function () {
         $("#cust-list").toggleClass("cus-hide");
         $("#arrowIcn").toggleClass("rotate");
         let o = $(this);
-        set_customer_detail(cus_option.index(o)-1);
+        set_customer_detail(cus_option.index(o) - 1);
       });
     }
   }
 };
-
-
-
 
 var refresh_item_cmb = function () {
   $("#item-list").empty();
@@ -42,14 +39,14 @@ var refresh_item_cmb = function () {
   <p>None</p>
 </li>`);
   for (let index = 0; index < items.length; index++) {
-    var raw =`<li class="item-option">
+    var raw = `<li class="item-option">
     <img
       src="${items[index].get_item_image()}"
       alt=""
     />
     <p>${items[index].get_item_name()} (${items[index].get_item_id()})</p>
-    </li>`
-     
+    </li>`;
+
     $("#item-list").append(raw);
     var item_option = $(".item-option");
 
@@ -60,25 +57,20 @@ var refresh_item_cmb = function () {
         $("#item-list").toggleClass("item-hide");
         $("#item-arrowIcn").toggleClass("rotate");
         var o = $(this);
-        set_item_detail(item_option.index(o)-1)
-        ;
+        set_item_detail(item_option.index(o) - 1);
       });
     }
   }
 };
 
-
-
-
-
-var get_index_number_by_item_id=function(id){
+var get_index_number_by_item_id = function (id) {
   for (let index = 0; index < items.length; index++) {
-    if(id==items[index].get_item_id()){
+    if (id == items[index].get_item_id()) {
       return index;
     }
   }
   return undefined;
-}
+};
 
 var set_customer_detail = function (index) {
   try {
@@ -155,18 +147,20 @@ let cart_data;
 // add to cart function
 var add_to_cart = function () {
   cart_data = {};
-  
-  let result = isExist($("#item-selectText").text().split("(")[1].split(")")[0]);
+
+  let result = isExist(
+    $("#item-selectText").text().split("(")[1].split(")")[0]
+  );
 
   if (result != undefined) {
-    let qty=parseInt($("#txt_ph_item_qty").val());
-    let unitPrice=parseInt($("#txt_ph_item_unit_price").val());
-    let dsc=parseInt($("#txt_ph_item_discount").val());
+    let qty = parseInt($("#txt_ph_item_qty").val());
+    let unitPrice = parseInt($("#txt_ph_item_unit_price").val());
+    let dsc = parseInt($("#txt_ph_item_discount").val());
 
     cart_ar[result].qty += parseInt($("#txt_ph_item_qty").val());
-    cart_ar[result].sub_total+= qty*unitPrice;
-    cart_ar[result].discount_in_rs+=(qty*unitPrice*dsc)/100;
-    cart_ar[result].total+=(qty*unitPrice)-(qty*unitPrice*dsc)/100;
+    cart_ar[result].sub_total += qty * unitPrice;
+    cart_ar[result].discount_in_rs += (qty * unitPrice * dsc) / 100;
+    cart_ar[result].total += qty * unitPrice - (qty * unitPrice * dsc) / 100;
   } else {
     cart_data.itemID = $("#item-selectText").text().split("(")[1].split(")")[0];
     cart_data.itemName = $("#item-selectText").text().split(" ")[0];
@@ -181,7 +175,9 @@ var add_to_cart = function () {
   reduce_quantity(
     parseInt($("#txt_ph_item_qty").val()),
     $("#txt_ph_item_stock").val(),
-    get_index_number_by_item_id($("#item-selectText").text().split("(")[1].split(")")[0])
+    get_index_number_by_item_id(
+      $("#item-selectText").text().split("(")[1].split(")")[0]
+    )
   );
   calculate_total(
     parseInt($("#txt_ph_item_qty").val()),
@@ -211,7 +207,7 @@ var calculate_total = function (qty, stk, dsc) {
 var reduce_quantity = function (qty, stk, index) {
   console.log(qty + " " + stk + " " + index);
   let crt = parseInt(stk) - parseInt(qty);
-  console.log("ss "+crt);
+  console.log("ss " + crt);
   items[index].set_item_quantity(crt);
   $("#txt_ph_item_stock").val(crt);
 };
@@ -237,7 +233,6 @@ var refresh_cart_table = function () {
 
   // cart item delete
   $("#tbl_cart>tr>td:nth-child(6)").on("click", function () {
-    
     for (let index = 0; index < cart_ar.length; index++) {
       if (
         cart_ar[index].itemID ==
@@ -249,7 +244,7 @@ var refresh_cart_table = function () {
         discount -= cart_ar[index].discount_in_rs;
         total -= cart_ar[index].total;
 
-        console.log("sub "+cart_ar[index].sub_total);
+        console.log("sub " + cart_ar[index].sub_total);
 
         $("#lbl_sub_total").text("Rs. " + sub_total + ".00");
         $("#lbl_discount").text("Rs. " + discount + ".00");
@@ -279,7 +274,9 @@ var refresh_cart_table = function () {
 
 var generate_order_id = function () {
   if (orders.length != 0) {
-    let lastID = parseInt(orders[orders.length - 1].get_order_id().split("O")[1]);
+    let lastID = parseInt(
+      orders[orders.length - 1].get_order_id().split("O")[1]
+    );
     if (lastID < 9) {
       return "O00" + (lastID + 1);
     } else if (lastID < 99) {
@@ -317,27 +314,26 @@ var clear_toatl = function () {
   $("#lbl_total").text("Rs. " + total + ".00");
 };
 
-
 function currentTime() {
-  let date = new Date(); 
+  let date = new Date();
   let hh = date.getHours();
   let mm = date.getMinutes();
   let ss = date.getSeconds();
   let session = "AM";
 
-  if(hh == 0){
-      hh = 12;
+  if (hh == 0) {
+    hh = 12;
   }
-  if(hh > 12){
-      hh = hh - 12;
-      session = "PM";
-   }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
 
-   hh = (hh < 10) ? "0" + hh : hh;
-   mm = (mm < 10) ? "0" + mm : mm;
-   ss = (ss < 10) ? "0" + ss : ss;
-    
-   let time = hh + ":" + mm + ":" + ss + " " + session;
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
+
+  let time = hh + ":" + mm + ":" + ss + " " + session;
 
   $("#clock").text(time);
   setTimeout(currentTime, 1000);
@@ -345,32 +341,50 @@ function currentTime() {
 currentTime();
 currentDate();
 
-function currentDate(){
+function currentDate() {
   let date = new Date();
-  let today = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate();
+  let today = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
   console.log(today);
   $("#date").text(today);
 }
 
-$("#pay_now").on("click",function(){
-
-  
+$("#pay_now").on("click", function () {
   txt_order_id;
   lbl_sub_total;
 
   let order1 = new Order();
   order1.set_order_id($("#txt_order_id").text());
   order1.set_cust_id($("#selectText").text().split("(")[1].split(")")[0]);
-  order1.set_date_time($("#date").text()+"  "+$("#clock").text());
-  order1.set_total_price($("#lbl_sub_total").text());
+  order1.set_date_time($("#date").text() + "  " + $("#clock").text());
+  order1.set_total_price($("#lbl_total").text());
 
   orders.push(order1);
 
   refresh_order_table();
-  console.log(generate_order_id());
   $("#txt_order_id").text(generate_order_id());
-
-  
+  clearAll();
 });
+
+var clearAll = function () {
+  cart_ar.splice(0, cart_ar.length);
+  sub_total = 0.0;
+  discount = 0.0;
+  total = 0.0;
+  $("#selectText").text("Select Customer");
+  $("#item-selectText").text("Select Customer");
+  $("#txt_ph_cust_name").val("");
+  $("#txt_ph_cust_address").val("");
+  $("#txt_ph_item_name").val("");
+  $("#txt_ph_item_stock").val("");
+  $("#txt_ph_item_unit_price").val("");
+  $("#txt_ph_item_discount").val("");
+  $("#txt_ph_item_qty").val("");
+
+  $("#tbl_cart").empty();
+
+  $("#lbl_sub_total").text("Rs.0.00");
+  $("#lbl_discount").text("Rs.0.00");
+  $("#lbl_total").text("Rs.0.00");
+};
 
 $("#txt_order_id").text(generate_order_id());
